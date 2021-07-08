6f58508051dd75dd85700384c9a5ddfc02ba4594
bindi <snair306@gmail.com>
2021-07-07 21:03:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2
+
+var random = Math.ceil(Math.random() +1)
+
+var num3 = 24;
+var num4 = 5;
+var mod = num3 % num4 
+
+var max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
